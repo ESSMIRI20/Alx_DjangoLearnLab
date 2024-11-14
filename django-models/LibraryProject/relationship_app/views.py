8e9afca02b2e499f468804f6232dc8480a29e8a5
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-11-14 17:47:07 +0100

    .\django-models\

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login  # Import login function
 
 # View for user registration
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login after successful registration
+            # Save the new user
+            user = form.save()
+            
+            # Automatically log in the user after registration
+            login(request, user)
+            
+            # Redirect to home or some other page after login
+            return redirect('home')  # Replace 'home' with your desired redirect target
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
-# Custom login view is handled by Django's LoginView, no need to create one unless customization is needed
-# The default LoginView works perfectly out of the box for simple authentication
-# You can customize it by specifying the template_name if needed
-# class LoginView(LoginView):
-#     template_name = 'relationship_app/login.html'
