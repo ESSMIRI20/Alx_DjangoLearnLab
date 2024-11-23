c65cc7d871fdc7b863d9bfba324ec8614ac3ef8a
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-11-23 14:21:20 +0100

    .\advanced_features_and_security\

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -12,13 +12,19 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 
-
+# accounts/admin.py
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["email", "username", "date_of_birth", "is_staff"]
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 class Book(models.Model):
@@ -8,17 +9,35 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+from django.conf import settings
 
-    class Meta:
-        permissions = [
-            ('can_view', 'Can view post'),
-            ('can_create', 'Can create post'),
-            ('can_edit', 'Can edit post'),
-            ('can_delete', 'Can delete post'),
-        ]
+# Example usage in another model:
+user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.title
+        return self.email
