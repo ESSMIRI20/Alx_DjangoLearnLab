cd703162de4be3de29e1a2e5eb00bed8a8896dfe
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-12-12 21:10:01 +0100

    django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,29 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Post CRUD URLs
-    path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    # Existing URL patterns for other views
 
-    # Comment CRUD URLs
-    path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),  # Corrected for creating comments
-    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='update_comment'),  # Corrected for updating comments
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),  # Corrected for deleting comments
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Other URLs...
-
-    # Search URL
-    path('search/', views.search, name='search'),
-
-    # Tag-based filtering
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
-    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='post_by_tag'),
+    # Comment-related URLs
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Post, Comment
 from .forms import CommentForm
-from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create View for Comment
 class CommentCreateView(LoginRequiredMixin, CreateView):
@@ -12,13 +12,13 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/add_comment.html'
 
     def form_valid(self, form):
-        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        post = get_object_or_404(Post, id=self.kwargs['pk'])  # Post ID from URL
         form.instance.post = post
-        form.instance.author = self.request.user
+        form.instance.author = self.request.user  # Associate comment with current user
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
+        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['pk']})
 
 # Update View for Comment
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -27,13 +27,11 @@ class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'blog/edit_comment.html'
 
     def get_queryset(self):
-        # Ensure users can only edit their own comments
-        return Comment.objects.filter(author=self.request.user)
+        return Comment.objects.filter(author=self.request.user)  # Only allow authors to edit their comments
 
     def test_func(self):
         comment = self.get_object()
-        # Only the comment author can edit the comment
-        return self.request.user == comment.author
+        return self.request.user == comment.author  # Ensure the user is the comment's author
 
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.object.post.id})
@@ -44,51 +42,11 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = 'blog/delete_comment.html'
 
     def get_queryset(self):
-        # Ensure users can only delete their own comments
-        return Comment.objects.filter(author=self.request.user)
+        return Comment.objects.filter(author=self.request.user)  # Only allow authors to delete their comments
 
     def test_func(self):
         comment = self.get_object()
-        # Only the comment author can delete the comment
-        return self.request.user == comment.author
+        return self.request.user == comment.author  # Ensure the user is the comment's author
 
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.object.post.id})
-
-from django.db.models import Q
-from .models import Post
-
-def search(request):
-    query = request.GET.get('q')
-    if query:
-        posts = Post.objects.filter(
-            Q(title__icontains=query) | 
-            Q(content__icontains=query) |
-            Q(tags__name__icontains=query)
-        ).distinct()
-    else:
-        posts = Post.objects.all()
-
-    return render(request, 'blog/search_results.html', {'posts': posts})
-
-from django.shortcuts import render
-from .models import Post, Tag
-
-def posts_by_tag(request, tag_name):
-    tag = Tag.objects.get(name=tag_name)
-    posts = tag.posts.all()
-    return render(request, 'blog/posts_by_tag.html', {'posts': posts, 'tag': tag})
-
-from django.views.generic import ListView
-from .models import Post
-from taggit.models import Tag
-
-class PostByTagListView(ListView):
-    model = Post
-    template_name = 'blog/post_list_by_tag.html'
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        tag_slug = self.kwargs.get('tag_slug')
-        tag = Tag.objects.get(slug=tag_slug)  # Fetch the tag based on the slug
-        return Post.objects.filter(tags=tag)  # Filter posts by the selected tag
