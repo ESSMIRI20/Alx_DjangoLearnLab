d56c610b5975e330b4f0c66c3e48e7082e3aedb3
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-12-12 20:24:05 +0100

    django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,83 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import CustomUserCreationForm
-
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in.')
-            return redirect('login')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'blog/register.html', {'form': form})
-
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .forms import ProfileUpdateForm, UserUpdateForm
-from django.contrib import messages
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, 'Your profile has been updated!')
-            return redirect('profile')
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form,
-    }
-    return render(request, 'blog/profile.html', context)
-
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-# ListView: List all posts
+# List all blog posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    ordering = ['-date_posted']  # Order posts by newest first
 
-# DetailView: Show individual post
+# Show a single blog post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
 
-# CreateView: Allow authenticated users to create a post
+# Create a new blog post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    template_name = 'blog/post_form.html'
     fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
 
-    # Automatically assign the current user as the author
     def form_valid(self, form):
-        form.instance.author = self.request.user
+        form.instance.author = self.request.user  # Set the current user as the author
         return super().form_valid(form)
 
-# UpdateView: Allow post authors to edit their post
+# Update an existing blog post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    template_name = 'blog/post_form.html'
     fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
 
-    # Ensure only the post author can edit it
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -86,13 +39,12 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         return self.request.user == post.author
 
-# DeleteView: Allow post authors to delete their post
+# Delete a blog post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('post_list')
 
-    # Ensure only the post author can delete it
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
