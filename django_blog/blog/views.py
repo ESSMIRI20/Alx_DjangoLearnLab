4e53c1fe0ade05cf2e672d86dcbf97360cf9c4f6
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-12-12 20:35:20 +0100

    django_blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseForbidden
 from .models import Post, Comment
 from .forms import CommentForm
 from django.urls import reverse_lazy
@@ -22,7 +21,7 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
 
 # Update View for Comment
-class CommentUpdateView(LoginRequiredMixin, UpdateView):
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/edit_comment.html'
@@ -31,17 +30,27 @@ class CommentUpdateView(LoginRequiredMixin, UpdateView):
         # Ensure users can only edit their own comments
         return Comment.objects.filter(author=self.request.user)
 
+    def test_func(self):
+        comment = self.get_object()
+        # Only the comment author can edit the comment
+        return self.request.user == comment.author
+
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.object.post.id})
 
 # Delete View for Comment
-class CommentDeleteView(LoginRequiredMixin, DeleteView):
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'blog/delete_comment.html'
 
     def get_queryset(self):
         # Ensure users can only delete their own comments
         return Comment.objects.filter(author=self.request.user)
+
+    def test_func(self):
+        comment = self.get_object()
+        # Only the comment author can delete the comment
+        return self.request.user == comment.author
 
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.object.post.id})
