44845d2fa05d61f0a2de0572398560f552b85e26
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-12-12 20:18:28 +0100

    django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,3 +8,12 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='blog/logged_out.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
+
+from django.urls import path
+from .views import PostCreateView, PostUpdateView, PostDeleteView
+
+urlpatterns = [
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
