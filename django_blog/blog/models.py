9e69dd32ae68da7e173a8a4ed431eb7df71ee722
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-12-12 21:08:44 +0100

    django_blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -22,3 +22,19 @@ class Post(models.Model):
     content = models.TextField()
     tags = TaggableManager()  # Use TaggableManager to handle tags
     # Other fields...
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from blog.models import Post  # Assuming Post model is in the same app
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # Link to Post model
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to User model (author of the comment)
+    content = models.TextField()  # Content of the comment
+    created_at = models.DateTimeField(default=timezone.now)  # Automatically set to current time
+    updated_at = models.DateTimeField(auto_now=True)  # Automatically set to current time when updated
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
+
