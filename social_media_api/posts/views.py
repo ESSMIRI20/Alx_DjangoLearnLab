150d8be4e49bf95b5af2376f567b51bc53a24b8e
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-12-14 21:53:55 +0100

    .\social_media_api\

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,35 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from rest_framework.filters import SearchFilter
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    filter_backends = [SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
 # posts/views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post
-from accounts.models import User
+from accounts.models import CustomUser  # Or User, depending on your model name
 from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
@@ -38,7 +11,7 @@ def user_feed(request):
     following_users = request.user.following.all()
     
     # Get posts from followed users, ordered by creation date (most recent first)
-    posts = Post.objects.filter(user__in=following_users).order_by('-created_at')
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
     
     # Serialize posts
     post_data = []
@@ -47,7 +20,7 @@ def user_feed(request):
             "id": post.id,
             "content": post.content,
             "created_at": post.created_at,
-            "user": post.user.username,
+            "author": post.author.username,  # Correct field for the user who created the post
         })
 
     return Response(post_data)
