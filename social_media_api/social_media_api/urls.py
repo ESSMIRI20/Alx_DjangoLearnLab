3a444cc2c35d21e5ab05668cad480d209685e141
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-12-14 21:25:30 +0100

    social_media_api

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -16,15 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
-
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('', include(router.urls)),
+    path('api/posts/', include('posts.urls')),
 ]
